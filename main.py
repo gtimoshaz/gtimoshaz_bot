9764c6f21ec67b5e193759a1b3d1e919b9f7159f
jDan735 <daniel734@bk.ru>
2021-07-24 23:08:53 +0300

    Add autopiner

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import utils
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
+POLL_CHAT = -1001472813343
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -15,6 +16,12 @@ async def send_bruh_signs_count(message: types.Message):
     """
     sc = utils.download_signs_count()
     await message.reply(str(sc))
+
+
+@dp.message_handler(content_types=["poll"])
+async def pin_poll(message):
+    if message.chat.id == POLL_CHAT:
+        await message.pin(disable_notification=True)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
